acfe738a8515a8aee6bc07a0adcb2ee168a5a52a
raliman22a-0077 <raliman22a-0077@olopsc.edu.ph>
2025-01-22 13:11:12 +0000

    s

---
+++ lib/pages/checklist_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChecklistPage extends StatefulWidget {
+  const ChecklistPage({super.key});
+
+  @override
+  State<ChecklistPage> createState() => _ChecklistPageState();
+}
+
+class _ChecklistPageState extends State<ChecklistPage> {
+  List<Map> checklist = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFF7E8C1),
+      body: Placeholder(),
+    );
+  }
+}
